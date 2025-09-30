66feb10ce5b66ffa627e87120b91842d69b6a58c
Faishalbhitex <fauzhidayat105@gmail.com>
2025-10-01 00:04:12 +0800

    updated: example streaming weather agent server

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { initChatModel } from "langchain/chat_models/universal";
 import { MultiServerMCPClient } from "@langchain/mcp-adapters";
 import { MemorySaver } from "@langchain/langgraph-checkpoint";
-
+import { z } from "zod";
+import { AIMessage } from "@langchain/core/messages";
 
 const googleApiKey = process.env.GOOGLE_API_KEY || "";
 const MCP_WEATHER_URL = process.env.MCP_WEATHER_KALTIM_URL || "";
@@ -18,33 +19,61 @@ const weatherMcpClient = new MultiServerMCPClient({
   }
 });
 
+const ResponseSchema = z.object({
+  status: z.enum(["butuh_informasi_tambahan", "selesai"]),
+  content: z.string(),
+});
+
 const PROMPT: string = `
 Kamu adalah agent cuaca di daerah provinsi Kalimantan timur hanya memberikan informasi seputar cuaca kota:
 samrinda, balikpapan, bontang, tenggarong dan sangatta.
-`
+`;
+
+const FORMAT_PROMPT = `
+Set status butuh_informasi_tambahan jika konteks yang diberikan user butuh lebih informasi atau diluar konteks dari tugas anda untuk melanjutkan tugasnya.
+Set status selesai jika anda tidak butuh konteks tambahan untuk mengerjakan tugasnnya dan menyelesaikannya.
+`;
+
 const llm = await initChatModel("google-genai:gemini-2.0-flash", {
   apiKey: googleApiKey,
 });
+
 const config = { configurable: { thread_id: "1" } };
 export const agent = createReactAgent({
   llm: llm,
   tools: await weatherMcpClient.getTools(),
   checkpointer: checkpointer,
   prompt: PROMPT,
+  responseFormat: {
+    prompt: FORMAT_PROMPT,
+    schema: ResponseSchema,
+  }
 });
+
+console.log("Waiting agent response..\n");
 
 const resp = await agent.invoke(
   {
     messages: [
       {
         role: "user",
-        content: "Hai! nama saya Faishal. Bagaimana cuaca di balikpapan sekarang?",
+        content: "Hai! nama saya Faishal. Bagaimana cuaca di Kaltim sekarang?",
       },
     ],
   },
   config
 );
-console.log("Agent Invoke:", resp.messages[resp.messages.length - 1].content);
+
+console.log("Agent response-1 (Invoke):", resp);
+console.log(
+  `\nAgent response-1 structured output: ${JSON.stringify(resp.structuredResponse, null, 2)}`
+);
+console.log(
+  `
+Status: ${resp.structuredResponse.status}
+Content: ${resp.structuredResponse.content}\n
+`
+);
 
 /*
 const resp2 = await agent.invoke(
@@ -69,14 +98,33 @@ const resp3Stream = await agent.stream(
       },
     ],
   },
-  { streamMode: "updates", ...config }
+  { streamMode: "values", ...config }
 );
 
-console.log("\nAgent Stream");
+console.log("\nAgent response-2 (stream):");
+let fullResponse = "";
+let structuredResponse2 = {};
 for await (const chunk of resp3Stream) {
   console.log(chunk);
   console.log("\n");
+  const lastMessage = chunk.messages[chunk.messages.length - 1] || [];
+  structuredResponse2 = chunk.structuredResponse;
+
+  if (lastMessage instanceof AIMessage) {
+    fullResponse = lastMessage.content || "";
+  }
 }
 
-
+console.log(
+  `Agent response-2 structured output: ${JSON.stringify(structuredResponse2, null, 2)}`
+);
+console.log(
+  `
+Status: ${structuredResponse2.status}
+Content: ${structuredResponse2.content}\n
+`
+);
+console.log(
+  `Full response-2 (stream): ${fullResponse}`
+);
 
